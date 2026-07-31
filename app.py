d16876081ced6b92cd5622f0eeea179bda3f6cf9
sohail435 <sohail435@users.noreply.github.com>
2026-07-31 11:32:51 +0500

    correction of predict and analyze differences

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.sidebar.markdown("Configure backend URLs (Render/Koyeb or local NodePort):")
 
 DEFAULT_CHURN_URL = st.secrets.get("CHURN_API_URL", "http://localhost:30080/predict")
 DEFAULT_CV_URL = st.secrets.get("RESIZER_API_URL", "http://localhost:30090/resize")
-DEFAULT_NLP_URL = st.secrets.get("SENTIMENT_API_URL", "http://localhost:30100/analyze")
+DEFAULT_NLP_URL = st.secrets.get("SENTIMENT_API_URL", "http://localhost:30100/predict")
 
 churn_url = st.sidebar.text_input("Churn Predictor Endpoint", DEFAULT_CHURN_URL)
 cv_url = st.sidebar.text_input("Image Resizer Endpoint", DEFAULT_CV_URL)
